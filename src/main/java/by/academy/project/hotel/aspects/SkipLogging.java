89862ee000d13adc5865c858ca3d7afcdcaaf6c1
Dzmitry_Betska <d.betska@gmail.com>
2023-09-10 21:18:02 +0300

    Writing javadoc documentation and creating README.md

--- src/main/java/by/academy/project/hotel/aspects/SkipLogging.java
+++ src/main/java/by/academy/project/hotel/aspects/SkipLogging.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**Annotation on excluding a method from registration in LoggingControllerAspect. Used above controller methods*/
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface SkipLogging {
